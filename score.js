883b29f2d693887b1c935eaecad8cf0bea10cc44
rajashree-guha <rajashree.625.guha@gmail.com>
2023-10-30 09:38:31 +0530

    Numeron2

--- score.js
+++ score.js
@@ -1,1 +1,12 @@
 // Iteration 8: Making scoreboard functional
+
+let params = new URLSearchParams(window.location.search)
+let score = params.get("score")
+
+let scoreBoard = document.getElementById("score-board")
+scoreBoard.textContent = score
+
+let playAgain = document.getElementById("play-again-button")
+playAgain.onclick = () =>{
+    window.location.href="./game.html"
+}
